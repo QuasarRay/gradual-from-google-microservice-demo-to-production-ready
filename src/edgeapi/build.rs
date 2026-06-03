326d744bb10c568cbc01bcecd8304812e780a73e
QuasarRay <quvazaray@gmail.com>
2026-06-03 13:49:33 +1000

    add build.rs

---
+++ src/edgeapi/build.rs
@@ -0,0 +1,9 @@
+// src/edge-api-rs/build.rs
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .compile_protos(&["proto/demo.proto"], &["proto"])?;
+
+    Ok(())
+}

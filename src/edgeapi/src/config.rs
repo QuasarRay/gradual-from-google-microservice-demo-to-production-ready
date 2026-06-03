e586f499e89ae312dd83133d7e6c3691e21d20e6
QuasarRay <quvazaray@gmail.com>
2026-06-03 19:56:55 +1000

    fixed config::config error

--- src/edgeapi/src/config.rs
+++ src/edgeapi/src/config.rs
@@ -1,4 +1,4 @@
-// src/edge-api-rs/src/config.rs
+use std::env;
 use serde::Deserialize;
 use summer::config::Configurable;
 
@@ -13,4 +13,43 @@ pub struct BackendConfig {
     pub shipping_addr: String,
     pub ad_addr: String,
     pub default_currency: String,
+}
+impl BackendConfig {
+    pub fn from_env() -> Self {
+        Self {
+            product_catalog_addr: env_or(
+                "PRODUCT_CATALOG_SERVICE_ADDR",
+                "http://productcatalogservice:3550",
+            ),
+            currency_addr: env_or(
+                "CURRENCY_SERVICE_ADDR",
+                "http://currencyservice:7000",
+            ),
+            cart_addr: env_or(
+                "CART_SERVICE_ADDR",
+                "http://cartservice:7070",
+            ),
+            recommendation_addr: env_or(
+                "RECOMMENDATION_SERVICE_ADDR",
+                "http://recommendationservice:8080",
+            ),
+            checkout_addr: env_or(
+                "CHECKOUT_SERVICE_ADDR",
+                "http://checkoutservice:5050",
+            ),
+            shipping_addr: env_or(
+                "SHIPPING_SERVICE_ADDR",
+                "http://shippingservice:50051",
+            ),
+            ad_addr: env_or(
+                "AD_SERVICE_ADDR",
+                "http://adservice:9555",
+            ),
+            default_currency: env_or("DEFAULT_CURRENCY", "USD"),
+        }
+    }
+}
+
+fn env_or(key: &str, default: &str) -> String {
+    env::var(key).unwrap_or_else(|_| default.to_string())
 }

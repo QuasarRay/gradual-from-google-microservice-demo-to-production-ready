9481e3caae763923257fafb3f66b709248286600
QuasarRay <quvazaray@gmail.com>
2026-06-03 14:04:11 +1000

    fixed protoc environment variable

--- src/edgeapi/build.rs
+++ src/edgeapi/build.rs
@@ -1,5 +1,11 @@
 // src/edge-api-rs/build.rs
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc_path = protoc_bin_vendored::protoc_bin_path()?;
+
+    unsafe {
+        std::env::set_var("PROTOC", protoc_path);
+    }
+
     tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
